a2dc62eeb083fa7d8a7ae6e8ad0376c197e3fd67
Ravi Kant Kumar <121558851+Ravikant420@users.noreply.github.com>
2026-07-29 02:10:38 +0530

    Add: permutation-string - submission-8

---
+++ Data Structures & Algorithms/permutation-string/submission-8.cpp
@@ -0,0 +1,27 @@
+// Brute Force
+class Solution {
+   public:
+    bool checkInclusion(string s1, string s2) {
+        // sort s1 for compare easily by sorting
+        sort(s1.begin(), s1.end());
+
+        int m = s1.length();
+
+        for (int i = 0; i < s2.length(); i++) {
+
+            for (int j = i; j < s2.length(); j++) {
+
+                // extract substring for compare
+                string subStr = s2.substr(i, m);
+
+                sort(subStr.begin(), subStr.end());
+
+                if (subStr == s1) {
+                    
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
